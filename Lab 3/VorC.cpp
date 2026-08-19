220a3dddbf29b456b54ab691f113c468d5f5e272
Piter Sharma <Sharmapeter07@gmail.com>
2026-08-20 01:12:52 +0545

    add vowel filter

---
+++ Lab 3/VorC.cpp
@@ -0,0 +1,23 @@
+//WAP to input a character and display whether it is vowel or consonant.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
+        cout << "It is a vowel." << endl;
+    }
+    else
+    {
+        cout << "It is a consonant." << endl;
+    }
+
+    return 0;
+}
