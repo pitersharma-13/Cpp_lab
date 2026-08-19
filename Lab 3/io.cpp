364aae454bee19bdd91c23d76d6a626ae3d6808e
Piter Sharma <Sharmapeter07@gmail.com>
2026-08-19 23:32:11 +0545

    add i/o functionality

---
+++ Lab 3/io.cpp
@@ -0,0 +1,22 @@
+// WAP to input age and display whether the person is eligible for voting or not.
+
+#include<iostream>
+using namespace std;
+int maoin ()
+{
+    int age ;
+    cout << " Enter your age " << endl;
+    cin >> age;
+
+    // using  the if else to check the condition
+    if ( age >= 18)
+    {
+        
+        cout << "You are elegible for the Voting in the Nepal"<<endl;
+    }
+    else 
+    {
+        cout << " You arenot elegible for the voting in the Nepal " <<endl;
+    }
+    return 0;
+}
