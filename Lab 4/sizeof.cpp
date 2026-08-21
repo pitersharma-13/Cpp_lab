30268b26a250c510c617e73be1bfbc8213a2a377
Piter Sharma <Sharmapeter07@gmail.com>
2026-08-21 10:09:07 +0545

    add sizeof code

---
+++ Lab 4/sizeof.cpp
@@ -0,0 +1,13 @@
+//Write a program to display the Size of theDifferent Datatypes in C.
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of the  int data_type " <<sizeof(int) <<endl;
+    cout<<"size of the  char data_type " <<sizeof(char) <<endl;
+    cout<<"size of the  float data_type " <<sizeof(float) <<endl;
+    cout<<"size of the  double data_type " <<sizeof(double) <<endl;    
+    cout<<"size of the  long data_type " <<sizeof(long) <<endl;    
+    cout<<"size of the  string data_type " <<sizeof(string) <<endl;    
+    return 0;
+}
